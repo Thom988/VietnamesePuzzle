868f3961bffc573e10310a3b93d8990f6f349918
Thom988 <t.quillet@hotmail.fr>
2024-04-04 12:49:42 +1100

    Mise en place du service et developpement composant game-data

--- src/app/core/services/combination.service.ts
+++ src/app/core/services/combination.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Combination } from '../models/combination.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CombinationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  generateCombinations(): Observable<number> {
+    return this.http.get<number>("http://localhost:9000/combinations/generate");
+  }
+
+  getCombinations(): Observable<Combination[]> {
+    return this.http.get<Combination[]>("http://localhost:9000/combinations");
+  }
+
+  deleteCombinations(): void {
+    this.http.delete("http://localhost:9000/combinations");
+  }
+
+  // deleteCombinations(): void {
+  //   this.http.delete("http://localhost:9000/combinations")
+  //     .subscribe(
+  //       () => {
+  //         console.log("La suppression s'est bien déroulée !");
+  //         // Tu peux ajouter d'autres actions ici si nécessaire
+  //       },
+  //       (error) => {
+  //         console.error("Erreur lors de la suppression :", error);
+  //         // Gère l'erreur ici (par exemple, affiche un message d'erreur à l'utilisateur)
+  //       }
+  //     );
+
+  getCombinationById(combinationId: number): Observable<Combination> {
+    return this.http.get<Combination>(`http://localhost:9000/combination/${combinationId}`);
+  }
+
 }
+
--- src/app/puzzle-game/game-data/game-data.component.ts
+++ src/app/puzzle-game/game-data/game-data.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Combination } from '../../core/models/combination.model';
+import { CombinationService } from '../../core/services/combination.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-game-data',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor, CommonModule],
   templateUrl: './game-data.component.html',
   styleUrl: './game-data.component.scss'
 })
 export class GameDataComponent {
+
+  executionTime$!: Observable<number | null>;
+  combinations$!: Observable<Combination[]>
+
+  constructor(private combinationService: CombinationService) {}
+
+  ngOnInit() {
+
+  }
+
+  generateCombinations() {
+    this.executionTime$ = this.combinationService.generateCombinations();
+    //this.combinations$ = this.combinationService.getCombinations();
+  }
+
+  deleteCombinations() {
+    this.combinationService.deleteCombinations();
+  }
+
 
 }
